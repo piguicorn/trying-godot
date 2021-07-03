1c4451d1b1ed526d4a060bb5b62e732d4f224137
Logan Benavente <logan.benavente@e2ycommerce.com>
2021-07-03 02:19:37 +0200

    trying, trying

--- src/Actors/Actors.gd
+++ src/Actors/Actors.gd
@@ -1,2 +1,12 @@
 extends KinematicBody2D
 class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 4000.0
+
+var _velocity: = Vector2.ZERO # how much the Player moves (x, y)
+
+	
+	
